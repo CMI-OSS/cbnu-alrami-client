0f5d7004e5ea4918acc3316b3ee315a999a76251
gijin noh <38103082+nohgijin@users.noreply.github.com>
2021-06-03 17:54:43 +0900

    Feature/24 schedule (#29)
    * chore: 가게 클릭시 상세 정보 다이얼로그 표시
    
    * chore: 학사일정 타임라인 높이 지정
    - 전체 영역이 회색으로 보이도록 수정
    
    * feat: 학사일정 페이지 api 제작, style 수정
    - get api 제작
    - 배경이 꽉 차게 style 수정
    
    * chore: 날짜를 변수로 제작

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,19 @@
+import dayjs from 'dayjs'
+
 import StyledHome from './style'
+import { Days } from '@src/constants'
 
 function Home() {
-  return <StyledHome>테스트</StyledHome>
+  const DAYS = [Days.Sunday, Days.Monday, Days.Thuesday, Days.Wednesday, Days.Thursday, Days.Friday, Days.Saturday]
+  const today = new Date()
+
+  return (
+    <StyledHome>
+      <div className="date">
+        {dayjs(today).month() + 1}월 {dayjs(today).date()}일 {DAYS[dayjs(today).day()]}요일
+      </div>
+    </StyledHome>
+  )
 }
 
 export default Home
